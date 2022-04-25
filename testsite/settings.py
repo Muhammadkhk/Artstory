43402450870e4c33ceb53b98622a12748c5c2cc3
Muhammadkhk <karizak98@gmail.com>
2022-04-25 16:25:19 +0430

    edited settings

--- testsite/settings.py
+++ testsite/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 import os
 from pathlib import Path
+import locale
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6+^hzge(@oyjrr0kxi2jey0pat#wp=d)qejavaa4r*#el+1aov'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
